c35ba04c6769e00045bebe3c9facd9c2ea860c05
Aavash69 <49222972+Aavash69@users.noreply.github.com>
2021-09-21 08:53:11 +0545

    Add files via upload

---
+++ question_model.py
@@ -0,0 +1,16 @@
+class Question:
+
+    def __init__(self, q_text, q_answer):
+        self.text = q_text
+        self.answer = q_answer
+
+
+
+# def police_check(age:int) -> bool:
+#     if age > 18:
+#         can_drive = True
+#     else:
+#         can_drive = False
+#     return can_drive
+
+
